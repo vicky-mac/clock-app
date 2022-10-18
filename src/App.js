8714b321d53fcc5eae075f62376992a5f4b2a2a0
vicky-mac <vickydrewmac@gmail.com>
2022-10-18 16:31:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "../src/style.css";
 function App() {
+  var today = new Date();
+  var curHr = today.getHours();
+  let result = "";
+
+  if (curHr < 12) {
+    result = "GOOD MORNING";
+  } else if (curHr < 18) {
+    result = "GOOD AFTERNOON";
+  } else {
+    result = "GOOD EVENING";
+  }
+
+  setInterval(updateTime, 1000);
+
+  let hour = new Date().getHours();
+  let seconds = new Date().getMinutes();
+  let ampm = hour >= 12 ? "pm" : "am";
+  let time = hour + ":" + seconds + ampm;
+  const [currentTime, setCurrentTime] = React.useState(time);
+
+  function updateTime() {
+    let hour = new Date().getHours();
+    let seconds = new Date().getMinutes();
+    let ampm = hour >= 12 ? "pm" : "am";
+    let newTime = hour + ":" + seconds + ampm;
+    setCurrentTime(newTime);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{result}, IT'S CURRENTLY</h1>
+      <p className="time">{currentTime}</p>
+      <h3>IN PONDICHERRY, INDIA</h3>
     </div>
   );
 }
